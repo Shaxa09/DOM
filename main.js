986085f54f2659cc6a20d9f6966168ba8b5b52bc
Shaxa09 <shaxaraxmatullaev@gmail.com>
2024-09-09 16:02:53 +0500

    snake

--- main.js
+++ main.js
@@ -1,60 +1,10 @@
-let timer
-let seconds = 0
-let run = false
+const boxes = document.querySelectorAll('.box')
 
-
-
-const numbersElement = document.getElementById('numbers')
-const startBtn = document.getElementById('start')
-const stopBtn = document.getElementById('stop')
-const resetBtn = document.getElementById('reset')
-
-
-
-
-const secundomer = () => {
-    const minute = Math.floor(seconds / 3600)
-    const secs = Math.floor((seconds % 3600) / 60)
-    const mili = seconds % 60
-
-    numbersElement.innerHTML = minute + ":" + secs + ":" + mili
-};
-
-
-
-
-const startTimer = () => {
-    if (run === false) {
-        timer = setInterval(() => {
-            seconds++
-            secundomer()
-        }, 10)
-        run = true
+boxes.forEach((box) => {
+    box.onmouseenter = () => {
+        box.classList.add('active')
     }
-};
-
-const stopTimer = () => {
-    clearInterval(timer)
-    run = false
-}
-
-const resetTimer = () => {
-    clearInterval(timer)
-    seconds = 0
-    secundomer()
-    run = false
-}
-
-
-
-startBtn.onclick = () => {
-    startTimer()
-}
-
-stopBtn.onclick = () => {
-    stopTimer()
-}
-
-resetBtn.onclick = () => {
-    resetTimer()
-}
+    box.onclick = () => {
+        box.classList.toggle('focus')
+    }
+})
